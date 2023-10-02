fad060e6845ef0122de1efb73557ae6d6ebe9a90
Mattia Buoso <94168690+MattiaBuoso@users.noreply.github.com>
2023-10-02 12:05:47 +0200

    aggiunti metodi dell'interfaccia

---
+++ DP_to_Bin/DP_to_Bin/ConvertMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_to_Bin
+{
+    public interface ConvertMethod
+    {
+        bool[] Convert_DP_to_Bin(int[] dp);
+        int Convert_DP_to_Int(int[] dp);
+        int Convert_Bin_to_Int(bool[] bn);
+        int[] Convert_Bin_to_DP(bool[] b);
+    }
+}
---
+++ DP_to_Bin/DP_to_Bin/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_to_Bin
+{
+    internal class Program : ConvertMethod
+    {
+        bool[] ConvertMethod.Convert_DP_to_Bin(int[] dp)
+        {
+            bool[] boolIP = new bool[32];
+
+            return boolIP;
+        }
+
+        int ConvertMethod.Convert_DP_to_Int(int[] dp)
+        {
+            int a = 0;
+            return a;
+        }
+
+        int ConvertMethod.Convert_Bin_to_Int(bool[] bn)
+        {
+            int a = 0;  
+            return a;
+        }
+
+        int[] ConvertMethod.Convert_Bin_to_DP(bool[] b)
+        {
+            int a = 0;  
+            return new int[32];
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] ip = new int[4];
+
+            ip = GetOctet();
+
+        }
+
+        static int[] GetOctet()
+        {
+            int[] ip = new int[4];
+            
+            for(int i = 0; i<4; i++)
+            {
+                try
+                {
+                    ip[i] = Convert.ToByte(Console.ReadLine());
+                }
+                catch
+                {
+                    throw new Exception("Invalid Element");
+                }
+            }
+
+            return ip; 
+        }
+    }
+}
